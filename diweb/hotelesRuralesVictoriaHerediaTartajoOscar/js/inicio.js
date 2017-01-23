0966f66ed653648fcee3a0896e5e99bc4f217cae
oschertar <oschertar@gmail.com>
2017-01-23 21:04:21 +0100

    Subida Práctica 12: Hoteles Rurales
    Subida Práctica 12: Hoteles Rurales

--- diweb/hotelesRuralesVictoriaHerediaTartajoOscar/js/inicio.js
+++ diweb/hotelesRuralesVictoriaHerediaTartajoOscar/js/inicio.js
@@ -1,7 +1,38 @@
-{
+{	
+	let capaCarga;
+
+	let bajarOpacidad = function(valor, duracion){
+		setTimeout(function(){ capaCarga.style.opacity = valor;}, duracion);
+		setTimeout(function(){ simboloCarga.style.opacity = valor;}, duracion);
+		capaCarga.style.transition = "1s all";
+		
+
+
+	}
+
+	let empezarCarga = function(){
+		bajarOpacidad(0.95,1000);
+		bajarOpacidad(0.85,1500);
+		bajarOpacidad(0.75,2000);
+		bajarOpacidad(0.65,2500);
+		bajarOpacidad(0.55,3000);
+		bajarOpacidad(0.45,3500);
+		bajarOpacidad(0.35,4000);
+		bajarOpacidad(0.30,4500);
+		bajarOpacidad(0.25,5000);
+		bajarOpacidad(0.2,5500);
+		bajarOpacidad(0.15,6000);
+		setTimeout(function(){ capaCarga.style.display = "none";}, 6400);
+		setTimeout(function(){ simboloCarga.style.display = "none";}, 6400);
+	}
+
+
 	let init = function(){
-		let capaCarga = document.getElementById("capaCargando");
-		setTimeout(function(){ capaCarga.style.display = "none";}, 5300);
+		capaCarga = document.getElementById("capaCargando");
+		simboloCarga = document.getElementsByClassName("sk-folding-cube");
+		
+		setTimeout(empezarCarga, 3000);
+		
 	}
 
 
