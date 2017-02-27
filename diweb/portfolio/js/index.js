5437a919ed27c916f56693d80a6a684fea1fef39
oschertar <oschertar@gmail.com>
2017-02-27 18:21:42 +0100

    Corregido error en js para móviles
    Corregido error en js para móviles

--- diweb/portfolio/js/index.js
+++ diweb/portfolio/js/index.js
@@ -1,14 +1,21 @@
 $(function(){
 	var my_image = new Image();
 	my_image.src = 'img/fondoAnimado.gif';
+
+
 	$("#bajar").on("click", function(event){
 		event.preventDefault();
-
+		var alto = $(window).height();
+            var ancho = $(window).width();
 		$("body").css("background-image", "url(img/fondoAnimado.gif)");
-		if($(window).width() >= 200 && $(window).width() < 780)
-			$("body").animate({backgroundSize: "500%"}, 5000);
-		else if($(window).width() >= 780)
+		if(alto > ancho){
+			if(ancho >= 200 && ancho < 780)
+				$("body").animate({backgroundSize: "500%"}, 5000);
+			else if(ancho >= 780)
+				$("body").animate({backgroundSize: "225%"}, 5000);
+		}else{
 			$("body").animate({backgroundSize: "225%"}, 5000);
+		}
 		/*$("#numeroPlanta").animate({marginTop: "45%", fontSize: "350%", width: "30%"}, 5000);
 		contador = 0;
 		setInterval(function(){
